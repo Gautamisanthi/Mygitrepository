07179bab5e1d89e992347f74fab56fdc40b54401
kpavi <kpavi@MSI>
2025-11-18 15:54:16 +0530

    depaartment

--- src/test/java/createOrg/createorg.java
+++ src/test/java/createOrg/createorg.java
@@ -7,13 +7,18 @@ public class createorg {
 
 		public void createOrg() {
 			System.out.println("Execute create org");
-			
 		}
+		
 		@Test
 		public void modifyorg() {
 			System.out.println("Execute modify org");
-			
 		}
+		@Test
+		public void departmentorg() {
+			System.out.println("Add departments");
+		}
+			
+		
 		
 		
 }
