c9cd1db22abc47391bb8eaff11613e2f7fe90dab
kpavi <kpavi@MSI>
2025-11-18 16:22:04 +0530

    department added

---
+++ src/test/java/createOrg/departmentaddedd.java
@@ -0,0 +1,12 @@
+package createOrg;
+
+import org.testng.annotations.Test;
+
+public class departmentaddedd {
+
+	@Test
+	public void departmentorg() {
+		System.out.println("Add departments");
+	}
+		
+}
