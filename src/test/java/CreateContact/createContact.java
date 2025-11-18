61bef6abf19d7230bce936a315c12e852b8d9580
gautami1988 <gautami1988@10.104.94.102>
2025-11-18 15:35:28 +0530

    change

---
+++ src/test/java/CreateContact/createContact.java
@@ -0,0 +1,18 @@
+package CreateContact;
+
+import org.testng.annotations.Test;
+
+@Test
+public class createContact {
+
+	public void createcontact() {
+		System.out.println("Execute create contact");
+		
+	}
+	@Test
+	public void modifyContact() {
+		System.out.println("Execute modify contact");
+		
+	}
+			
+}
